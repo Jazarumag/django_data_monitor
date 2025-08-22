871ca894caa17e5099e4583c10a0b42387ec8e28
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2025-08-22 17:50:51 -0500

    Update wsgi.py

--- backend_analytics_server/wsgi.py
+++ backend_analytics_server/wsgi.py
@@ -10,8 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_analytics_server.settings')
 
 application = get_wsgi_application()
+
+# Usar settings.STATIC_ROOT para asegurar la ruta correcta
+if not settings.DEBUG:
+    application = WhiteNoise(application, root=settings.STATIC_ROOT)
