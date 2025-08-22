b9ae250822a0d0953d13b57190cdc150e27f9b86
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2025-08-22 17:53:52 -0500

    Update wsgi.py

--- backend_analytics_server/wsgi.py
+++ backend_analytics_server/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_analytics_server.settings')
